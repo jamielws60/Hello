5f49446a3e8b0def9a54b8f57397615fa6df449c
Jamie Lewis <jamielws60@gmail.com>
2017-10-27 00:01:37 -0230

    Changes for hello

--- lib/hello.py
+++ lib/hello.py
@@ -1,6 +1,2 @@
-# Default is World
-# Author: Jim Weirich (jim@somewhere.com)
 import sys
-
-name = sys.argv[1] if len(sys.argv) > 1 else 'World'
-print 'Hello, %s!' % name
+print "Hello, %s!" % sys.argv[1]
